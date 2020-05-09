84ad0ac460f4853b848b0a7c3db389e56b4478a4
Eugene Ostashko <ostashko4@gmail.com>
2020-05-09 17:56:57 +0300

    Error screen stack trace scroll bar
    Signed-off-by: ReliableCauliflower <ostashko4@gmail.com>

--- lib/screens/error_screen.dart
+++ lib/screens/error_screen.dart
@@ -1,11 +1,14 @@
 import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
+import 'package:moleculis/widgets/flexible_scrollbar.dart';
 
 class ErrorScreen extends StatelessWidget {
   final FlutterErrorDetails details;
   final bool showStacktrace;
 
-  const ErrorScreen({Key key, this.details, this.showStacktrace})
+  final ScrollController stackTraceController = ScrollController();
+
+  ErrorScreen({Key key, this.details, this.showStacktrace})
       : super(key: key);
 
   @override
@@ -57,17 +60,22 @@ class ErrorScreen extends StatelessWidget {
       List<String> stackTrace = details.stack.toString().split("\n");
       return SizedBox(
         height: 200.0,
-        child: ListView.builder(
-          padding: EdgeInsets.all(8.0),
-          itemCount: stackTrace.length,
-          itemBuilder: (BuildContext context, int index) {
-            String line = stackTrace[index];
-            if (line?.isNotEmpty == true) {
-              return Text(line);
-            } else {
-              return SizedBox();
-            }
-          },
+        child: FlexibleScrollbar(
+          key: GlobalKey(),
+          controller: stackTraceController,
+          child: ListView.builder(
+            controller: stackTraceController,
+            padding: EdgeInsets.all(8.0),
+            itemCount: stackTrace.length,
+            itemBuilder: (BuildContext context, int index) {
+              String line = stackTrace[index];
+              if (line?.isNotEmpty == true) {
+                return Text(line);
+              } else {
+                return SizedBox();
+              }
+            },
+          ),
         ),
       );
     } else {
